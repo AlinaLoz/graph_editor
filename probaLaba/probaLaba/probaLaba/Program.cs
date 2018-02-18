6ad169fbc37b68df6f629a37c1c02e9a05bdd923
Алина Лозюк <alina-lozyuk@mail.ru>
2018-02-18 20:20:29 +0300

    Changed project template to WindowsForms

---
+++ probaLaba/probaLaba/probaLaba/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace probaLaba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(2, 3);
+            Console.ReadKey();
+        }
+
+        class Calculator {
+            public static void Add(int x, int y){
+                int z = x + y;
+                Console.WriteLine();   
+}
+        }
+    }
+}
